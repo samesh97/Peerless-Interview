4162254b22b0b8a5c79258f40dacf090f93d6e05
Samesh Alahakoon <samesha@msalesapp.com>
2026-02-05 15:32:25 +0800

    Removed User Entity.

--- src/Data/ApplicationDbContext.cs
+++ src/Data/ApplicationDbContext.cs
@@ -5,7 +5,6 @@ namespace PeerlessInterview.src.Data;
 
 public class ApplicationDbContext : DbContext
 {
-    public DbSet<User> Users { get; set; }
     public DbSet<Customer> Customers { get; set; }
 
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
